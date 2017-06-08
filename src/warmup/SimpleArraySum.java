4a7adb5be36d57d6961927bfc5dfcade7d0a041c
Burak Şahin <suspicioususer>
2017-06-08 16:53:12 +0300

    Simple Array Sum

---
+++ src/warmup/SimpleArraySum.java
@@ -0,0 +1,22 @@
+package warmup;
+
+import java.util.Scanner;
+
+public class SimpleArraySum {
+	
+	public static void main(String[] args) {
+		int size;
+		Scanner scanner = new Scanner(System.in);
+		size = scanner.nextInt();
+		int[] test = new int[size];
+		for (int i = 0; i < size; i++) {
+			test[i] = scanner.nextInt();
+		}
+		int temp = 0;
+		for (int i : test) {
+			temp += i;
+		}
+		System.out.println(temp);
+		scanner.close();
+	}
+}
