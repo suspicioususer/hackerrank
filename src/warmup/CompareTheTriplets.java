1037b906b1c22ba16e32d82ac1b15a00dcd44e05
Burak Şahin <suspicioususer>
2017-06-08 16:53:25 +0300

    Compare the Triplets

---
+++ src/warmup/CompareTheTriplets.java
@@ -0,0 +1,32 @@
+package warmup;
+
+import java.util.Scanner;
+
+public class CompareTheTriplets {
+	public static void main(String[] args) {
+		int alice = 0, bob = 0;
+		Scanner scanner = new Scanner(System.in);
+		int[] A = new int[3];
+		int[] B = new int[3];
+		
+		for (int i = 0; i < 3; i++) {
+			A[i] = scanner.nextInt();
+		}
+
+		for (int i = 0; i < 3; i++) {
+			B[i] = scanner.nextInt();
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (A[i] < B[i]) {
+				bob++;
+			} else if (A[i] > B[i]) {
+				alice++;
+			}
+		}
+
+		System.out.println(alice + " " + bob);
+		scanner.close();
+	}
+
+}
