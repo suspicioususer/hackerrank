c88b57cbf27551b664e8e34ca78f0e65ad2e3f5f
suspicioususer <sahinburak47@gmail.com>
2020-05-20 20:21:57 +0300

    Divisible Sum Pairs

---
+++ src/implementation/DivisibleSumPairs.java
@@ -0,0 +1,27 @@
+package implementation;
+
+public class DivisibleSumPairs {
+
+	public static int divisibleSumPairs(int n, int k, int[] ar) {
+		int result = 0;
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = i + 1; j < ar.length; j++) {
+				if ((ar[i] + ar[j]) % k == 0) {
+					result++;
+				}
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int n = 6;
+		int k = 3;
+
+		int[] ar = { 1, 3, 2, 6, 1, 2 };
+
+		int result = divisibleSumPairs(n, k, ar);
+		System.out.println("Result:" + result);
+	}
+
+}
